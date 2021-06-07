4dab491cef4d8428a48f85b5dca0534d436d877f
juliengossa <julien.gossa@gmail.com>
2021-06-07 20:37:32 +0200

    Portage de la fonction tree pour tous les codes

--- legiplot_functions.R
+++ legiplot_functions.R
@@ -16,41 +16,68 @@ legiplot_loadall_short <- function() {
 # Arbres
 legiplot_tree <- function(lecode) {
   
-  data <- lp_stats_lts1 %>%
+  lp_tree <<- lp_stats_lts1 %>%
     filter(code==lecode) %>%
-    filter(partie != "Partie réglementaire") %>%
+    filter(partie %in% c("Partie législative","Partie législative nouvelle") | code == "code civil") %>%
+    mutate(
+      livre = fct_recode(livre, "Sans Livre" = ""),
+      titre = fct_recode(titre, "Sans Titre" = ""),
+      chapitre = fct_recode(chapitre, "Sans Chapitre" = "")
+    ) %>%
     mutate(across(where(is.factor), as.character)) %>%
-    mutate(sous_partie = ifelse(sous_partie=="", partie,sous_partie)) 
-    
-  edges <- bind_rows(
-    data %>% group_by(from=code, to=sous_partie) %>% summarise(niveau = 5, groupe="code"),
-    data %>% group_by(from=sous_partie, to=livre) %>% summarise(niveau = 4, groupe=unique(sous_partie)),
-    data %>% group_by(from=livre, to=paste(livre,titre)) %>% summarise(niveau = 3, groupe=unique(sous_partie))
+    mutate(sous_partie = ifelse(sous_partie=="", partie, sous_partie)) 
+
+  if(n_distinct(lp_tree$sous_partie) > 1) {
+    edges <- bind_rows(
+      lp_tree %>% group_by(from=code, to=sous_partie) %>% summarise(niveau = 5, groupe="code"),
+      lp_tree %>% group_by(from=sous_partie, to=paste(sous_partie,livre)) %>% summarise(niveau = 4, groupe=unique(sous_partie))
+    )
+  } else {
+    edges <-
+      lp_tree %>% group_by(from=code, to=paste(sous_partie,livre)) %>% summarise(niveau = 5, groupe="code")
+  }
+  
+  edges <- bind_rows(edges,
+    lp_tree %>% group_by(from=paste(sous_partie,livre), to=paste(livre,titre)) %>% summarise(niveau = 3, groupe=unique(livre))
   )
   
   vertices <- bind_rows(
-    data %>% group_by(name = code) %>% summarise(label = unique(code), groupe = "code", niveau = 10, nb_articles = sum(nb_articles)),
-    #data %>% group_by(name = partie) %>% summarise(label = partie, groupe = partie, niveau = 5, nb_articles = sum(nb_articles)),
-    data %>% group_by(name = sous_partie) %>% summarise(label = unique(sous_partie), groupe = unique(sous_partie), niveau = 4, nb_articles = sum(nb_articles)),
-    data %>% group_by(name = livre) %>% summarise(label = unique(livre), groupe = unique(sous_partie), niveau = 3, nb_articles = sum(nb_articles)),
-    data %>% group_by(name = paste(livre,titre)) %>% summarise(label = unique(titre), groupe = unique(sous_partie), niveau = 2, nb_articles = sum(nb_articles))
+    lp_tree %>% group_by(name = code) %>% summarise(vlabel = unique(code), groupe = "code", niveau = 8, nb_articles = sum(nb_articles)),
+    #lp_tree %>% group_by(name = partie) %>% summarise(vlabel = partie, groupe = partie, niveau = 5, nb_articles = sum(nb_articles)),
+    #lp_tree %>% group_by(name = sous_partie) %>% summarise(vlabel = unique(sous_partie), groupe = unique(sous_partie), niveau = 4, nb_articles = sum(nb_articles)),
+    lp_tree %>% group_by(name = paste(sous_partie,livre)) %>% summarise(vlabel = unique(livre), groupe = unique(livre), niveau = 3, nb_articles = sum(nb_articles)),
+    lp_tree %>% group_by(name = paste(livre,titre)) %>% summarise(vlabel = unique(titre), groupe = unique(livre), niveau = 2, nb_articles = sum(nb_articles))
   )
   
-  #vertices %>% group_by(name) %>% summarise(n = n()) %>% arrange(desc(n))
+  if(n_distinct(lp_tree$sous_partie) > 1) {
+    vertices <- bind_rows(vertices,
+      lp_tree %>% group_by(name = sous_partie) %>% summarise(vlabel = unique(sous_partie), groupe = unique(sous_partie), niveau = 4, nb_articles = sum(nb_articles))
+    )
+  }
+  
+  if (nrow(vertices) > 100) {
+    edges <- edges %>% filter(niveau>3)
+    vertices <- vertices %>% filter(niveau>2)
+  }
+  
+  v <<- vertices
+  e <<- edges
+  lecode <<- lecode
+  #vertices %>% group_by(name) %>% summarise(n = n()) %>% arrange(desc(n)) %>% View()
   
   labeller = label_wrap_gen(width = 15)
   
-  mygraph<- graph_from_data_frame(edges,vertices=vertices) 
+  mygraph <<- graph_from_data_frame(edges,vertices=vertices)
   ggraph(mygraph, layout = 'dendrogram', circular =TRUE)+
     geom_edge_diagonal(aes(colour=groupe))+
     #scale_edge_colour_distiller(palette = "RdPu")+
     geom_node_point(aes(size=nb_articles,colour=groupe))+
-    geom_node_text(aes(label=labeller(label)),colour="white",size=vertices$niveau)+
+    geom_node_text(aes(label=labeller(vlabel)),colour="white",size=vertices$niveau)+
     scale_size_continuous(range=c(10,80)) +
     theme_void()+
     theme(legend.position="none",plot.margin=unit(c(0,0,0,0),"cm"))
 }
 
-legiplot_loadall_short()
-legiplot_tree("code de l'éducation")
+#legiplot_loadall_short()
+#legiplot_tree("code de l'éducation")
 
